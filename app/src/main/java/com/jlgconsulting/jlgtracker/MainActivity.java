0fe9e68098c8bcc10405e1d60c9efdb6ececbf97
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-02 16:49:56 +0200

    dev: start activity

--- app/src/main/java/com/jlgconsulting/jlgtracker/MainActivity.java
+++ app/src/main/java/com/jlgconsulting/jlgtracker/MainActivity.java
@@ -4,13 +4,18 @@ import static android.text.Html.FROM_HTML_MODE_COMPACT;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
                 "<p>You can configure the URL and the delay D.</p>";
         textView.setText(Html.fromHtml(htmlText, FROM_HTML_MODE_COMPACT));
 
+        continueButton = findViewById(R.id.button);
+    }
 
+    public void gotoNextActivity(View view) {
+        Log.d("JLGModule", "click");
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
     }
 }
