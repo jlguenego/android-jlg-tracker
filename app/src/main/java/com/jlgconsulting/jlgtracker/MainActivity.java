87347b1d1a09dd75362714f9f9f1e4e9dd20475f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-15 10:48:52 +0200

    dev: adding important notice.

--- app/src/main/java/com/jlgconsulting/jlgtracker/MainActivity.java
+++ app/src/main/java/com/jlgconsulting/jlgtracker/MainActivity.java
@@ -1,14 +1,11 @@
 package com.jlgconsulting.jlgtracker;
 
-import static android.text.Html.FROM_HTML_MODE_COMPACT;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,10 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        String htmlText = "<p>This app sends your geoloc to a URL every D seconds.</p>" +
-                "<p>You can configure the URL and the delay D.</p>";
-        textView.setText(Html.fromHtml(htmlText, FROM_HTML_MODE_COMPACT));
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/intro.html");
 
         pr = new PermissionRequest(this);
     }
@@ -42,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
         pr.askForBattery((result) -> {
             Log.d(TAG, "ask for battery result: " + result);
             if (result.getResultCode() != Activity.RESULT_OK) {
-                Toast.makeText(getApplicationContext(),"I need battery optimization set to unrestricted.",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "I need battery optimization set to unrestricted.", Toast.LENGTH_LONG).show();
                 return;
             }
             pr.askForPermissions((permissionResult) -> {
                 Log.d(TAG, "ask for permissions: " + permissionResult);
                 if (permissionResult.containsValue(false)) {
-                    Toast.makeText(getApplicationContext(),"I need all permissions set to unrestricted.",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "I need all permissions set to unrestricted.", Toast.LENGTH_LONG).show();
                     return;
                 }
 
